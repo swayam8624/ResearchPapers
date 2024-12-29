fa609df5020d054a93fc071b0afcd49721c08cd4
swayam singal <swayam.singal@gmail.com>
2024-12-29 19:59:02 +0530

    batch size change

--- Attentionallyouneed/config.py
+++ Attentionallyouneed/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 32,
+        "batch_size": 16,
         "num_epochs": 20,
         "lr": 10**-4,
         "seq_len": 350,
